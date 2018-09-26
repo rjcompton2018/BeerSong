ba2e79d859c248c5d46bcb3b9cfa33ceaaa53f55
Richard <richardjozefc@gmail.com>
2018-09-26 14:25:16 -0400

    Finished the code for BeerSong.java and got the program to work. It prints out the correct output with no errors. Just need to clean up the code and fix a few comments and it will be ready to submit.

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -2,25 +2,30 @@ public class BeerSong {
 
     //I am testing the commit feature of Git.
 
-    public void Main() {
+    public static void main(String [] args) {
+
+        Ninety_Nine_Bottles_of_Beer();
 
     }
 
-    public void Ninety_Nine_Bottles_of_Beer() {
+    public static void Ninety_Nine_Bottles_of_Beer() {
 
         String s = "s";
         for (int beers = 99; beers > -1;) {
-            System.out.print(beers + "bottle" + s + " of beer on the wall, ");
+            System.out.print(beers + " bottle" + s + " of beer on the wall, ");
             System.out.println(beers + " bottle" + s + " of beer");
             if (beers == 0) {
-                System.out.print("No more left");
-                System.out.println("99 bottles of beer on the wall.\n");
+                System.out.print("Take one down, pass it around, no more bottles of beer on the wall.");
+                System.out.println("\n\n****END OF PROGRAM OUTPUT****");
                 System.exit(0);
             }
             else {
                 System.out.println("Take one down, pass it around, ");
+                s = (--beers == 1) ? "" : "s";
+                System.out.println(beers + " bottle" + s + " of beer on the wall.\n");
+                  }
             }
         }
 
     }
-}
+

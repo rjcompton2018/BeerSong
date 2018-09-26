9659ba1dcf1d4f85350174125af25c93a9a4e02b
Richard <richardjozefc@gmail.com>
2018-09-26 14:12:38 -0400

    Updated BeerSong.java to include a bit of code. The functionality of the program is nearly complete, simply need to figure out some logic.

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -8,5 +8,19 @@ public class BeerSong {
 
     public void Ninety_Nine_Bottles_of_Beer() {
 
+        String s = "s";
+        for (int beers = 99; beers > -1;) {
+            System.out.print(beers + "bottle" + s + " of beer on the wall, ");
+            System.out.println(beers + " bottle" + s + " of beer");
+            if (beers == 0) {
+                System.out.print("No more left");
+                System.out.println("99 bottles of beer on the wall.\n");
+                System.exit(0);
+            }
+            else {
+                System.out.println("Take one down, pass it around, ");
+            }
+        }
+
     }
 }

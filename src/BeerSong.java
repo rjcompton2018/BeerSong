47ce2dec07f4e09ec111a782ac0a9a90f872cd0e
Richard <richardjozefc@gmail.com>
2018-09-26 15:47:08 -0400

    Completely finished coding, commenting, and cleaning BeerSong.java. Both methods function and the code outputs the correct information. All that is left to do is merge the implementation and interface branches to the master branch and submit the assignment. I have successfully learned how to use Git and GitHub during this assignment.

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,24 +1,37 @@
 public class BeerSong {
 
-    //I am testing the commit feature of Git.
+    //Richard Compton
+    //CSI 3370
+    //99 Bottles of Beer on the Wall Song
+    /* In this project, I learn how to use Git and GitHub, and code the 99 bottles
+    of beer on the walls song.
+     */
 
     public static void main(String [] args) {
 
+        //Calls the 99 Bottles of Beer method
         Ninety_Nine_Bottles_of_Beer();
 
     }
 
     public static void Ninety_Nine_Bottles_of_Beer() {
 
+        //Variable used to add 's' to the end of 'beer' when needed
         String s = "s";
-        for (int beers = 99; beers > -1;) {
+
+        //For loop that creates the 99 bottles of beer song
+        for (int beers = 99; beers >= -1;) {
             System.out.print(beers + " bottle" + s + " of beer on the wall, ");
             System.out.println(beers + " bottle" + s + " of beer");
+
+            //The end of the song
             if (beers == 0) {
                 System.out.print("Take one down, pass it around, no more bottles of beer on the wall.");
                 System.out.println("\n\n****END OF PROGRAM OUTPUT****");
                 System.exit(0);
             }
+
+            //The continuation of the song
             else {
                 System.out.println("Take one down, pass it around, ");
                 s = (--beers == 1) ? "" : "s";
@@ -26,6 +39,5 @@ public class BeerSong {
                   }
             }
         }
-
     }
 
